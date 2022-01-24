e20dc3807f64783f2ac3967faf9a38dbc9fc530c
Fabio Spampinato <spampinabio@gmail.com>
2022-01-24 02:25:00 +0000

    From: avoiding calling a noop cleanup function unnecessarily

--- src/from.ts
+++ src/from.ts
@@ -2,16 +2,15 @@
 /* IMPORT */
 
 import oby from '.';
-import {NOOP} from './constants';
 import {IDisposer, IObservableWithoutInitial} from './types';
 
 /* MAIN */
 
 const from = <T> ( fn: ( observable: IObservableWithoutInitial<T> ) => IDisposer | void ): IObservableWithoutInitial<T> => {
 
-  const disposer = () => cleanup ();
+  const disposer = () => cleanup && cleanup ();
   const observable = oby<T> ( undefined, disposer );
-  const cleanup = fn ( observable ) || NOOP;
+  const cleanup = fn ( observable );
 
   return observable;
 
