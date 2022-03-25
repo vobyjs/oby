6332f467065ed9bca16a983cce7e93d63dc99d5a
Fabio Spampinato <spampinabio@gmail.com>
2022-03-25 11:05:46 +0000

    Added a cleanup benchmark script

---
+++ tasks/cleanup.js
@@ -0,0 +1,49 @@
+
+/* IMPORT */
+
+const {default: $} = require ( '../dist' );
+
+/* MAIN */
+
+const o = $(-1);
+
+$.root ( dispose => {
+
+  const isSelected = $.selector ( o );
+
+  const disposers = [];
+
+  for ( let i = 0, l = 1000; i < l; i++ ) {
+
+    $.root ( dispose => {
+
+      disposers.push ( dispose );
+
+      $.effect ( () => {
+        o ();
+        isSelected ( o );
+      });
+
+      $.effect ( () => {
+        o ();
+      });
+
+      $.effect ( () => {
+        o ();
+      });
+
+    });
+
+  }
+
+  console.time ( 'cleanup' );
+
+  dispose ();
+
+  disposers.forEach ( dispose => dispose () );
+
+  console.timeEnd ( 'cleanup' );
+
+});
+
+debugger;
