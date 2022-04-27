be9de02e5198b7ba31f549007b21b8214eb999b7
Fabio Spampinato <spampinabio@gmail.com>
2022-04-27 20:50:08 +0100

    $.ternary: accepting conditions of any type

--- src/methods/ternary.ts
+++ src/methods/ternary.ts
@@ -7,7 +7,7 @@ import type {ObservableReadonly, FunctionMaybe, Resolved} from '~/types';
 
 /* MAIN */
 
-const ternary = <T, F> ( when: FunctionMaybe<boolean>, valueTrue: T, valueFalse: F ): ObservableReadonly<Resolved<T | F>> => {
+const ternary = <T, F> ( when: FunctionMaybe<unknown>, valueTrue: T, valueFalse: F ): ObservableReadonly<Resolved<T | F>> => {
 
   return computed ( () => {
 
