16c28188539023c543b6302ed34c3e72e372c8bf
Fabio Spampinato <spampinabio@gmail.com>
2022-04-27 20:49:31 +0100

    $.if: accepting conditions of any type

--- src/methods/if.ts
+++ src/methods/if.ts
@@ -6,7 +6,7 @@ import type {ObservableReadonly, FunctionMaybe, Resolved} from '~/types';
 
 /* MAIN */
 
-const _if = <T> ( when: FunctionMaybe<boolean>, value: T ): ObservableReadonly<Resolved<T | undefined>> => {
+const _if = <T> ( when: FunctionMaybe<unknown>, value: T ): ObservableReadonly<Resolved<T | undefined>> => {
 
   return ternary ( when, value, undefined );
 
