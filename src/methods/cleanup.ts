2b1c53e0d1220303eb26bcb664a9c280050632c1
Fabio Spampinato <spampinabio@gmail.com>
2022-06-06 17:38:15 +0100

    $.cleanup: doing nothing when the current owner is the super owner

--- src/methods/cleanup.ts
+++ src/methods/cleanup.ts
@@ -1,12 +1,14 @@
 
 /* IMPORT */
 
-import {OWNER} from '~/constants';
+import {OWNER, SUPER_OWNER} from '~/constants';
 import type {CleanupFunction, Callable} from '~/types';
 
 /* MAIN */
 
 const cleanup = ( fn: Callable<CleanupFunction> ): void => {
+
+  if ( OWNER.current === SUPER_OWNER ) return;
 
   OWNER.current.registerCleanup ( fn );
 
