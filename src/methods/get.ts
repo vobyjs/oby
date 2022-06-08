10b46e0d29666989c48193789c6852ea3aff27b5
Fabio Spampinato <spampinabio@gmail.com>
2022-06-08 12:45:52 +0100

    $.get: added a comment

--- src/methods/get.ts
+++ src/methods/get.ts
@@ -6,6 +6,8 @@ import type {ObservableReadonly} from '~/types';
 
 /* MAIN */
 
+//TODO: Maybe add a "resolveFunction" argument
+
 const get = <T> ( value: T ): T extends ObservableReadonly<infer U> ? U : T => {
 
   if ( isObservable ( value ) ) return value () as any; //TSC
