244b2f27cc3cf2f0a5fe6478845f0816ab4c7547
Fabio Spampinato <spampinabio@gmail.com>
2022-10-14 17:40:37 +0100

    $.target: simplified implementation

--- src/methods/target.ts
+++ src/methods/target.ts
@@ -1,19 +1,23 @@
 
 /* IMPORT */
 
-import {SYMBOL_OBSERVABLE, SYMBOL_OBSERVABLE_FROZEN} from '~/constants';
-import ObservableClass from '~/objects/observable';
+import {SYMBOL_OBSERVABLE} from '~/constants';
+import isObservable from '~/methods/is_observable';
 import type {IObservable, Observable, ObservableReadonly} from '~/types';
 
 /* MAIN */
 
 const target = <T> ( observable: Observable<T> | ObservableReadonly<T> | IObservable<T> ): IObservable<T> => {
 
-  if ( observable instanceof ObservableClass ) return observable;
+  if ( isObservable ( observable ) ) {
 
-  if ( SYMBOL_OBSERVABLE_FROZEN in observable ) throw new Error ( 'Impossible' );
+    return observable ( SYMBOL_OBSERVABLE as any ) as any; //TSC
 
-  return observable ( SYMBOL_OBSERVABLE as any ) as any; //TSC
+  } else {
+
+    return observable;
+
+  }
 
 };
 
