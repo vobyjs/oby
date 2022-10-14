d361f272ff1371701f61f103544c842fc5ace739
Fabio Spampinato <spampinabio@gmail.com>
2022-10-14 16:28:00 +0100

    $.context: slightly cleaned up code

--- src/methods/context.ts
+++ src/methods/context.ts
@@ -9,11 +9,11 @@ function context <T> ( symbol: symbol ): T | undefined;
 function context <T> ( symbol: symbol, value: T ): undefined;
 function context <T> ( symbol: symbol, value?: T ) {
 
-  if ( arguments.length < 2 ) { // Read
+  if ( arguments.length < 2 ) {
 
     return OWNER.current.read<T> ( symbol );
 
-  } else { // Write
+  } else {
 
     return OWNER.current.write ( symbol, value );
 
