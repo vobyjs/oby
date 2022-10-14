f0c3783c2dac543d26aa1d7457b403b0d526cb15
Fabio Spampinato <spampinabio@gmail.com>
2022-10-14 16:20:18 +0100

    $.cleanup: simplified implementation, allowing cleanups to be registered on the superroot too

--- src/methods/cleanup.ts
+++ src/methods/cleanup.ts
@@ -1,14 +1,12 @@
 
 /* IMPORT */
 
-import {OWNER, SUPER_OWNER} from '~/constants';
+import {OWNER} from '~/constants';
 import type {CleanupFunction, Callable} from '~/types';
 
 /* MAIN */
 
 const cleanup = ( fn: Callable<CleanupFunction> ): void => {
-
-  if ( OWNER.current === SUPER_OWNER ) return;
 
   OWNER.current.registerCleanup ( fn );
 
