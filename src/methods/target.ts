4c4f3aa9ff662ab725f2d8579e6489ebd9aeb742
Fabio Spampinato <spampinabio@gmail.com>
2022-10-31 16:09:58 +0000

    $.target: simplified internal implementation slightly

--- src/methods/target.ts
+++ src/methods/target.ts
@@ -2,14 +2,14 @@
 /* IMPORT */
 
 import {SYMBOL_OBSERVABLE} from '~/constants';
-import isObservable from '~/methods/is_observable';
+import {isFunction} from '~/utils';
 import type {IObservable, Observable, ObservableReadonly} from '~/types';
 
 /* MAIN */
 
 const target = <T> ( observable: Observable<T> | ObservableReadonly<T> | IObservable<T> ): IObservable<T> => {
 
-  if ( isObservable ( observable ) ) {
+  if ( isFunction ( observable ) ) {
 
     return observable ( SYMBOL_OBSERVABLE as any ) as any; //TSC
 
