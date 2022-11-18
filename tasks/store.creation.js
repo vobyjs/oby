321b63a4955caa7cf4c1e0137d7cd9d85a038777
Fabio Spampinato <spampinabio@gmail.com>
2022-11-18 14:58:48 +0000

    $.store: added a little benchmark measuring creation time

---
+++ tasks/store.creation.js
@@ -0,0 +1,14 @@
+
+/* IMPORT */
+
+import $ from '../dist/index.js';
+
+/* MAIN */
+
+const arr = new Array ( 1_000_000 ).fill ( 0 ).map ( () => ({}) );
+
+console.time ( 'creation' );
+for ( let i = 0, l = arr.length; i < l; i++ ) {
+  $.store ( arr[i] );
+}
+console.timeEnd ( 'creation' );
