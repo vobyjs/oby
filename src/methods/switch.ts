890e373638cdcb9cdc2a72e4f16785264842a661
Fabio Spampinato <spampinabio@gmail.com>
2023-05-04 17:24:40 +0100

    $.switch: optimized to create as few memos as possible, which sometimes mean 0 memos, rather than 2

--- src/methods/switch.ts
+++ src/methods/switch.ts
@@ -3,13 +3,33 @@
 
 import {OBSERVABLE_UNDEFINED} from '~/constants';
 import get from '~/methods/get';
+import isObservable from '~/methods/is_observable';
 import isObservableFrozen from '~/methods/is_observable_frozen';
 import memo from '~/methods/memo';
 import resolve from '~/methods/resolve';
 import untrack from '~/methods/untrack';
 import warmup from '~/methods/warmup';
-import {is} from '~/utils';
+import {frozen} from '~/objects/callable';
+import {is, isArray, isFunction} from '~/utils';
 import type {ObservableReadonly, FunctionMaybe, Resolved} from '~/types';
+
+/* HELPERS */
+
+const match = <T, R, F> ( condition: T, values: ([T, R] | [R])[], fallback?: F ): R | F | undefined => {
+
+  for ( let i = 0, l = values.length; i < l; i++ ) {
+
+    const value = values[i];
+
+    if ( value.length === 1 ) return value[0];
+
+    if ( is ( value[0], condition ) ) return value[1];
+
+  }
+
+  return fallback;
+
+};
 
 /* MAIN */
 
@@ -19,25 +39,21 @@ function _switch <T, R> ( when: FunctionMaybe<T>, values: [T, R][], fallback?: u
 function _switch <T, R, F> ( when: FunctionMaybe<T>, values: [T, R][], fallback: F ): ObservableReadonly<Resolved<R | F>>;
 function _switch <T, R, F> ( when: FunctionMaybe<T>, values: ([T, R] | [R])[], fallback?: F ): ObservableReadonly<Resolved<R | F | undefined>> {
 
-  const value = warmup ( memo ( () => {
+  const value = isFunction ( when ) ? warmup ( memo ( () => match ( when (), values, fallback ) ) ) : match ( when, values, fallback );
 
-    const condition = get ( when );
+  if ( !isFunction ( value ) && !isArray ( value ) ) {
 
-    for ( let i = 0, l = values.length; i < l; i++ ) {
+    return frozen ( value );
 
-      const value = values[i];
+  } else if ( !isObservable ( value ) ) {
 
-      if ( value.length === 1 ) return value[0];
+    return memo ( () => {
 
-      if ( is ( value[0], condition ) ) return value[1];
+      return resolve ( value );
 
-    }
+    });
 
-    return fallback;
-
-  }));
-
-  if ( isObservableFrozen ( value ) ) {
+  } else if ( isObservableFrozen ( value ) ) {
 
     const valueUnwrapped = untrack ( value );
 
