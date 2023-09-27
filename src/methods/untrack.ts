08f4fc4cd8a14c356d9176452c61108ae709212e
Fabio Spampinato <spampinabio@gmail.com>
2023-09-27 13:55:24 +0100

    untrack: improved types, accounting for FunctionMaybe types explicitly, which TS seems to need

--- src/methods/untrack.ts
+++ src/methods/untrack.ts
@@ -3,10 +3,11 @@
 
 import {OBSERVER, setObserver} from '~/context';
 import {isFunction} from '~/utils';
-import type {UntrackFunction} from '~/types';
+import type {FunctionMaybe, UntrackFunction} from '~/types';
 
 /* MAIN */
 
+function untrack <T> ( fn: FunctionMaybe<T> ): T;
 function untrack <T> ( fn: UntrackFunction<T> ): T;
 function untrack <T> ( fn: T ): T;
 function untrack <T> ( fn: UntrackFunction<T> | T ) {
